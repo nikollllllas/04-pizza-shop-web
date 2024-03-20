f0c303f69a42bbb8c06474dbe39121c57b8e82f4
nikollllllas <nikollasohta2014@gmail.com>
2024-03-20 15:59:33 -0300

    initial config

---
+++ src/pages/app/dashboard.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from "react-helmet-async";
+
+export function Dashboard() {
+  return (
+    <div>
+      <Helmet title="Dashboard" />
+      <h1>SignIn</h1>;
+    </div>
+  );
+}
---
+++ src/routes.tsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import { AppLayout } from "./pages/_layouts/app";
+import { Dashboard } from "./pages/app/dashboard";
+import { AuthLayout } from "./pages/_layouts/auth";
+import { SignIn } from "./pages/auth/sign-in";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [{ path: "/", element: <Dashboard /> }],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [{ path: "/", element: <SignIn /> }],
+  },
+]);
