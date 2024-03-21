0bb5a0c588c6f964da932ad435af75262e028807
nikollllllas <nikollasohta2014@gmail.com>
2024-03-21 12:48:55 -0300

    feat: login via e-mail and toast

--- src/app.tsx
+++ src/app.tsx
@@ -3,11 +3,13 @@ import './global.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
+import { Toaster } from 'sonner'
 
 export function App() {
   return (
     <HelmetProvider>
       <Helmet titleTemplate='%s | pizza.shop' />
+      <Toaster richColors closeButton />
       <RouterProvider router={router} />
     </HelmetProvider>
   )
