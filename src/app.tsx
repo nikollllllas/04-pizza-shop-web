958cb8d83711b6b1804e8ac10e1bae062d025f87
nikollllllas <nikollasohta2014@gmail.com>
2024-03-21 16:00:59 -0300

    feat: switch themes

--- src/app.tsx
+++ src/app.tsx
@@ -4,13 +4,18 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { Toaster } from 'sonner'
+import { ThemeProvider } from '@/components/theme/theme-provider'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate='%s | pizza.shop' />
-      <Toaster richColors closeButton />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme='dark' storageKey='pizzashop-theme'>
+        <Helmet titleTemplate='%s | pizza.shop' />
+
+        <Toaster richColors closeButton />
+
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
