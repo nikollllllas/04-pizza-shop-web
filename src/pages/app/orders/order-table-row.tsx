4f95cac27d1b82a6f0fea42e623af69189767f2b
nikollllllas <nikollasohta2014@gmail.com>
2024-04-04 10:40:45 -0300

    feat: consuming API data

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -3,8 +3,21 @@ import { Dialog, DialogTrigger } from '@/components/ui/dialog'
 import { TableRow, TableCell } from '@/components/ui/table'
 import { ArrowRight, Search, X } from 'lucide-react'
 import { OrderDetails } from './order-details'
+import { OrderStatus } from './order-status'
+import { formatDistanceToNow } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 
-export function OrderTableRow() {
+interface OrderTableRowProps {
+  order: {
+    orderId: string
+    createdAt: string
+    status: 'pending' | 'canceled' | 'processing' | 'delivering' | 'delivered'
+    customerName: string
+    total: number
+  }
+}
+
+export function OrderTableRow({ order }: OrderTableRowProps) {
   return (
     <TableRow>
       <TableCell>
@@ -18,16 +31,20 @@ export function OrderTableRow() {
           <OrderDetails />
         </Dialog>
       </TableCell>
-      <TableCell className='font-mono text-xs font-medium'>123</TableCell>
-      <TableCell className='text-muted-foreground'>Há 15 minutos</TableCell>
-      <TableCell>
-        <div className='flex items-center gap-2'>
-          <span className='h-2 w-2 rounded-full bg-slate-400'></span>
-          <span className='font-medium text-muted-foreground'>Pendente</span>
-        </div>
+      <TableCell className='font-mono text-xs font-medium'>{order.orderId}</TableCell>
+      <TableCell className='text-muted-foreground'>
+        {formatDistanceToNow(order.createdAt, {
+          locale: ptBR,
+          addSuffix: true
+        })}
       </TableCell>
-      <TableCell className='font-medium'>Nikollas Ohta</TableCell>
-      <TableCell className='font-medium'>R$ 119,90</TableCell>
+      <TableCell>
+        <OrderStatus status={order.status} />
+      </TableCell>
+      <TableCell className='font-medium'>{order.customerName}</TableCell>
+      <TableCell className='font-medium'>
+        {order.total.toLocaleString('pt-BR', { style: 'currency', currency: 'brl' })}
+      </TableCell>
       <TableCell>
         <Button variant={'outline'} size={'xs'}>
           <ArrowRight className='mr-2 h-3 w-3' />
